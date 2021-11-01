bae9f873b7e7dfbeaf918e8fe497fe7b1de2e5bb
behers10 <behers10@github.com>
2021-11-01 15:11:21 +0530

    frontend2

--- frontend/app.py
+++ frontend/app.py
@@ -1,34 +1,20 @@
 from flask import Flask, jsonify, request, session, redirect, url_for, g, Response
 from flask.templating import render_template
-#from ml_utils import predict_credit, retrain, explain_model
-import sys
-import io
-import nltk
-import pickle
+import requests
+import json
 import os
-import numpy as np
-from nltk.stem import WordNetLemmatizer
-#nltk.download('wordnet')
-import re
-from lime import lime_text
-from sklearn.pipeline import make_pipeline
-from lime.lime_text import LimeTextExplainer
+from datetime import datetime
 
 
-#-------------TBD
-NLPmodel=pickle.load(open('NLPmodel.sav','rb'))
-vectorizer=pickle.load(open('vectorizer.sav','rb'))
-encoder=pickle.load(open('encoder.sav','rb'))
-c = make_pipeline(vectorizer, NLPmodel)
-#-----------------
 app = Flask(__name__)
-SECRET_KEY = os.urandom(24)
+#SECRET_KEY = os.urandom(24)
 
 query = {}
-
+global accu , model, dt
+accu , model ,dt = "","",""
 @app.route('/', methods = ['GET'])
 def home():
-    return render_template('index.html', message = "Click on the links to go the respective pages")
+    return render_template('home.html', message = "Click on the links to go the respective pages")
 
 
 @app.route('/about', methods = ["GET"])
@@ -39,33 +25,35 @@ def about():
 @app.route('/predict', methods =['GET', 'POST'])
 def predict():
     exp,predict = "",""
+    qry={}
     if request.method == 'GET':
         message = "Let us predict"
         return render_template('predict.html', message = message)
     else:
         news = request.form['entry'] 
         print(news)
-        predict = 'business'
-        explainer = LimeTextExplainer(class_names=encoder.classes_)
-        class_names=encoder.classes_
-        exp = explainer.explain_instance(news, c.predict_proba, num_features=len(encoder.classes_), top_labels=2)
-        print(exp.available_labels())
-        exp = exp.as_html()
-        return render_template('predict.html', predict=predict, exp=exp)
+        qry['news']=news
+        predict = requests.post(" http://trainer:3000/predict",json=qry)
+        result=predict.json()
+        try:  exp=result['exp'] 
+        except :  exp=""
+        return render_template('predict.html', entry=news,predict=result['result'], exp=result['exp'])
 
 
 @app.route('/retrain',  methods =['GET', 'POST'])
 def Retrain():
-
+    global accu , model , dt
     if request.method == 'GET':
-        return render_template('retrain.html')
+        return render_template('retrain.html',accu=accu, algo=model,dt=dt)
     else:
-        accu=86
-        algo='Logistic Regression'
-
-        return render_template('retrain.html',accu=accu, algo=algo) 
+        retrain=requests.get("http://trainer:3000/train?update=false")
+        result=retrain.json()
+        accu=result['Naive_Bayes']
+        model='Naive_Bayes'
+        dt=str(datetime.now())
+        return render_template('retrain.html',accu=accu, algo=model,dt=dt) 
 
 
 if __name__ == '__main__':  
-    app.secret_key = SECRET_KEY
-    app.run(port=5000, debug=True)
+    #app.secret_key = SECRET_KEY
+    app.run(host='0.0.0.0',port=8000)
